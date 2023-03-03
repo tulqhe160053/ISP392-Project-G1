ecb6b9f9582fa99cd703aa6486dcdad99fac53a1
thainxhe161309@fpt.edu.vn <>
2023-03-03 15:57:30 +0700

    edit ProductDAO

--- src/java/Dao/ProductDAO.java
+++ src/java/Dao/ProductDAO.java
@@ -261,6 +261,9 @@ public class ProductDAO extends MyDAO implements DAOInterface<Product> {
     }
     
     
+    
+    
+    
     @Override
     public void insert(Product t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
