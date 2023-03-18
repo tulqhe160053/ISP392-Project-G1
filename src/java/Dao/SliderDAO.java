5a4fd707997433dcf0d9cdfae1fc26baaf3ef5de
thanhhnhe161545 <thanhhnhe161545@fpt.edu.vn>
2023-03-18 15:53:50 +0700

    commit

--- src/java/Dao/SliderDAO.java
+++ src/java/Dao/SliderDAO.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  * @author Tu
  */
 public class SliderDAO extends MyDAO implements DAOInterface<Slider> {
-
+    
     @Override
     public ArrayList<Slider> selectAll() {
         ArrayList<Slider> t = new ArrayList<>();
@@ -39,26 +39,49 @@ public class SliderDAO extends MyDAO implements DAOInterface<Slider> {
         }
         return (t);
     }
+
+    public Slider selectByID(String id) {
+        xSql = "select * from Slider where id = ? ";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int sid = rs.getInt("ID");
+                String urlImage = rs.getString("urlImage");
+                int catId = rs.getInt("CatID");
+                CategoryDAO catDao = new CategoryDAO();
+                Category cat = catDao.selectById(new Category(catId, null));
+                String backLink = rs.getString("BackLink");
+                return new Slider(sid, urlImage, cat, backLink);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     
-     public int countSlider(){
-        int count = 0 ;
+    public int countSlider() {
+        int count = 0;
         String sql = "select count(*) as counts from slider";
         try {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 count = rs.getInt(1);
             }
         } catch (Exception e) {
         }
         return count;
     }
-     
-     public void InsertSlider(String imageLink, String catid ) {
+    
+    public void InsertSlider(String imageLink, String catid) {
         try {
-            String sql = "insert into dbo.[Slider] \n" +
-                    "(urlImage, CatID) \n" +
-                    "values (?,?)";
+            String sql = "insert into dbo.[Slider] \n"
+                    + "(urlImage, CatID) \n"
+                    + "values (?,?)";
             ps = con.prepareStatement(sql);
             ps.setString(1, imageLink);
             ps.setString(2, catid);
@@ -67,50 +90,50 @@ public class SliderDAO extends MyDAO implements DAOInterface<Slider> {
             e.printStackTrace();
         }
     }
-     
-        public void deleteSlider(int id) {
-            String query = "delete from slider where id = ?";
-            try{
-                ps = con.prepareStatement(query);
-                ps.setInt(1, id);
-                ps.executeUpdate();
-            } catch (Exception e){
-            }
+    
+    public void deleteSlider(int id) {
+        String query = "delete from slider where id = ?";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
     }
-
+    
     @Override
     public Slider selectById(Slider t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     @Override
     public void insert(Slider t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     @Override
     public int insertAll(ArrayList<Slider> arr) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     @Override
     public void delete(Slider t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     @Override
     public int deleteAll(ArrayList<Slider> arr) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     @Override
     public void update(int x, Slider t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     public static void main(String[] args) {
-        SliderDAO sds = new SliderDAO();
-        ArrayList<Slider> list = sds.selectAll();
-        System.out.println(list.get(0).getUrlImage());
+        SliderDAO dao = new SliderDAO();
+        Slider s = dao.selectByID("1");
+        System.out.println(s);
     }
 }
